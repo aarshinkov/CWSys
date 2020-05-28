3ad246ae290c9fe233fc74d68a6d605da48a0f5d
Atanas Arshinkov <a.arshinkov97@gmail.com>
2020-05-28 03:31:27 +0300

    Added login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+    CoursesComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
